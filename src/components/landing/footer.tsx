ba88a6eea7bbf6ef3418ed0610ee2a3a042b7683
Rushikesh Kulkarni <kulkarnirushikesh905@gmail.com>
2025-12-28 01:37:54 +0530

    footer logo addition

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -64,7 +64,7 @@ export function Footer() {
             <img 
               src="/growsuite-logo.png" 
               alt="GrowSuite" 
-              className="h-14 w-auto"
+              className="h-16 w-auto"
             />
           </a>
         </div>
